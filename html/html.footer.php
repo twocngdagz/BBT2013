43ffadfff64910ef9fbaeceeba988ec8baee64cb
unknown <twocngdagz@yahoo.com>
2013-08-14 10:07:36 +0800

    commit

--- html/html.footer.php
+++ html/html.footer.php
@@ -305,6 +305,37 @@
 		</div>
 	</div>
 </div>
+<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 6000,
+  width: 216,
+  height: 240,
+  theme: {
+    shell: {
+      background: '#ffffff',
+      color: '#292b2c'
+    },
+    tweets: {
+      background: '#f6f6f6',
+      color: '#292b2c',
+      links: '#1388d2'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: false,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: false,
+    behavior: 'all'
+  }
+}).render().setUser('babybeddingtown').start();
+</script>
 <script>if (window.Stripe) $("#checkout-cc-form").show()</script>
 <noscript>
 	<p>JavaScript is required for the registration form.</p>
