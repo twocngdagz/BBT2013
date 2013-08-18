3a98cdb4b87dd04c31aacd8eae4d196abbb692fd
unknown <twocngdagz@yahoo.com>
2013-08-19 00:32:30 +0800

    commit

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 	ini_set('display_errors', 'On');
-	ini_set('output_buffering', 32768);
 	error_reporting(E_ALL);
-	echo ini_get('output_buffering');
 	include('mchn.config.php');
 	include('main.php');
 ?>
