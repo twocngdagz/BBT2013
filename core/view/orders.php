98ccccaf7770ef8d1c00aa9d321c40826c95a02a
unknown <twocngdagz@yahoo.com>
2013-09-26 16:05:07 +0800

    commit

--- core/view/orders.php
+++ core/view/orders.php
@@ -124,9 +124,8 @@
 						<input type="submit" class="btn btn-success" value="Add" id="submitter" name="button"/>
 						<input type="submit" class="btn btn-danger" value="Delete" id="delete" name="delete" style="display: none"/>
 					</div>
-				</form>
-			</div>
-			
+				</div>
+			</form>
 			<div class="tab-pane" id="order_items">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -250,221 +249,220 @@
 				height : 270
 			});
 
-//on click grid from product to product options
-$('.flexOrders').click(function(event){       
-	$('.trSelected', this).each( function(){
-		$.ajax({
-			type:'POST',
-			url:"core/view/grid/orders.php",
-			data:"id="+$(this).attr('id').substr(3),
-			success:function(data) {
-				$.each(data.rows, function (index, value) {
-					console.log('Mederic');
-					var options = $(".flexUpdateOrders").flexigrid(	{
-						url : 'core/view/grid/order_details.php',
-						dataType : 'json',
-						colModel : [ 
-						{ display : 'Id', name : 'id', width : 15, sortable : true, align : 'right'}, 
-						{ display : 'Name', name : 'option_name', width : 180, sortable : true, align : 'left'}, 
-						{ display : 'Description', name : 'option_description', width : 280, sortable : true, align : 'left'},
-						{ display : 'Price', name : 'price', width : 60, sortable : true, align : 'left'},
-						{ display : 'Weight', name : 'weight', width : 60, sortable : true, align : 'left'}
-						],
-						searchitems : [ {
-							display : 'Id',
-							name : 'id'
-						}, {
-							display : 'Option Name',
-							name : 'option_name',
-							isdefault : false
-						}, {
-							display : 'Option Description',
-							name : 'option_description',
-							isdefault : false
-						}, {
-							display : 'Price',
-							name : 'price',
-							isdefault : false
-						}],
-						onDoubleClick: false,
-						multiSel: false,
-						sortname : "id",
-						sortorder : "desc",
-						usepager : true,
-						title : '',
-						useRp : true,
-						resize: false,
-						rp : 10,
-						query: value.cell.id,
-						qtype :'`products_item_options`.`product_id`',
-						showTableToggleBtn : true,
-						width : 700,
-						onSuccess: function(data) {
+			//on click grid from product to product options
+			$('.flexOrders').click(function(event){       
+				$('.trSelected', this).each( function(){
+					$.ajax({
+						type:'POST',
+						url:"core/view/grid/orders.php",
+						data:"id="+$(this).attr('id').substr(3),
+						success:function(data) {
+							$.each(data.rows, function (index, value) {
+								console.log('Mederic');
+								var options = $(".flexUpdateOrders").flexigrid(	{
+									url : 'core/view/grid/order_details.php',
+									dataType : 'json',
+									colModel : [ 
+									{ display : 'Id', name : 'id', width : 15, sortable : true, align : 'right'}, 
+									{ display : 'Name', name : 'option_name', width : 180, sortable : true, align : 'left'}, 
+									{ display : 'Description', name : 'option_description', width : 280, sortable : true, align : 'left'},
+									{ display : 'Price', name : 'price', width : 60, sortable : true, align : 'left'},
+									{ display : 'Weight', name : 'weight', width : 60, sortable : true, align : 'left'}
+									],
+									searchitems : [ {
+										display : 'Id',
+										name : 'id'
+									}, {
+										display : 'Option Name',
+										name : 'option_name',
+										isdefault : false
+									}, {
+										display : 'Option Description',
+										name : 'option_description',
+										isdefault : false
+									}, {
+										display : 'Price',
+										name : 'price',
+										isdefault : false
+									}],
+									onDoubleClick: false,
+									multiSel: false,
+									sortname : "id",
+									sortorder : "desc",
+									usepager : true,
+									title : '',
+									useRp : true,
+									resize: false,
+									rp : 10,
+									query: value.cell.id,
+									qtype :'`products_item_options`.`product_id`',
+									showTableToggleBtn : true,
+									width : 700,
+									onSuccess: function(data) {
+									},
+									singleSelect: true,
+									height : 270
+								});
+								options.flexReload();
+							});
 						},
-						singleSelect: true,
-						height : 270
+						dataType:'json'
 					});
-options.flexReload();
-});
-},
-dataType:'json'
-});
 
-$.ajax({
-	type:'POST',
-	url:"core/view/grid/order_customer_details.php",
-	data:"id="+$(this).attr('id').substr(3),
-	success:function(data) {
-		$.each(data.rows, function (index, value) { 
-			$('#first_name').attr('value', value.cell.first_name);
-			$('#last_name').attr('value', value.cell.last_name);
-			$('#email').attr('value', value.cell.email);
-			$('#phone').attr('value', value.cell.phone);
-			$('#address').attr('value', value.cell.address);
-			$('#address2').attr('value', value.cell.address2);
-			$('#city').attr('value', value.cell.city);
-			$('#zipcode').attr('value', value.cell.zipcode);
-		});
-	},
-	dataType:'json'
-});
+					$.ajax({
+						type:'POST',
+						url:"core/view/grid/order_customer_details.php",
+						data:"id="+$(this).attr('id').substr(3),
+						success:function(data) {
+							$.each(data.rows, function (index, value) { 
+								$('#first_name').attr('value', value.cell.first_name);
+								$('#last_name').attr('value', value.cell.last_name);
+								$('#email').attr('value', value.cell.email);
+								$('#phone').attr('value', value.cell.phone);
+								$('#address').attr('value', value.cell.address);
+								$('#address2').attr('value', value.cell.address2);
+								$('#city').attr('value', value.cell.city);
+								$('#zipcode').attr('value', value.cell.zipcode);
+							});
+						},
+						dataType:'json'
+					});
 
-$('#products-id').attr('value', $(this).attr('id').substr(3));
-$('#delete').show();
+					$('#products-id').attr('value', $(this).attr('id').substr(3));
+					$('#delete').show();
 
-$('#submitter').attr('value', 'Update');
-$('#action').attr('value', 'update');
-$('#modal-create-orders').modal('show');
-});
-
-
-});
-
-$('#modal-create-orders').on('shown', function () {
-	
-	if( $('#submitter').attr('value') == 'Add' || $('#submitter').attr('value') == 'Create' ) {
-		$('#order-details-select').hide();
-		$('#order-items-select').hide();
-		$('#myTab li:eq(0) a').tab('show');
-		$('#myTab li:eq(1) a').tab('hide');
-		$('#myTab li:eq(2) a').tab('hide');
-	} else if( $('#submitter').attr('value') == 'Update') {
-		$('#order-details-select').show();
-		$('#order-items-select').show();
-	}
-})
-
-$('#modal-create-orders').on('hidden', function () {
-	window.location.reload();
-	$('#myTab li:eq(0) a').tab('show');
-	
-	$('#submitter').attr('value', 'Create');		
-	$('#user-id').attr('value', '');
-	$('#action').attr('value', 'create');
-	$('#delete').hide();
-	
-	$('#name_short').attr('value', '');
-	$('#name_long').attr('value', '');
-	$('#description_long').text('');
-	$('#description_short').attr('value', '');
-	
-	$('#image_thumbnail_val').attr('src', '');
-	$('#image_small_val').attr('src', '');
-	$('#image_large_val').attr('src', '');
-	
-	$('#thumbnail_current').attr('value', '');
-	$('#small_current').attr('value', '');
-	$('#large_current').attr('value', '');
-	
-	$('#image_small_link span').html('');
-	$('#image_large_link span').html('');
-	$('#image_thumbnail_link span').html('');
-	
-	$('#comments').attr('value', '');
-	$('#name_long').attr('value', '');
-	$('#comments').text('');
-	$('#zip').attr('value', '');
-	$('#country').attr('value',  '');
-	
-	$('#status').val('');
-	
-	$('#price').attr('value', '');
-	
-	$('#category_id').val('');
-	$('#brand_id').val('');
-	$('#vendor_id').val('');
-});
-
-//on click product options 		
-$('.flexProductOptions').click(function(event){
-	console.log('Tab clicked');
-	$('.trSelected', this).each( function(){	
-		$.ajax({
-			type:'POST',
-			url:"core/view/grid/productOptions.php",
-			data:"id="+$(this).attr('id').substr(3),
-			success:function(data) {
-				$.each(data.rows, function (index, value) {	
-					$('#option_name').attr('value', value.cell.option_name);
-					$('#option_description').attr('value', value.cell.option_description);
-					
-					$('#image_thumbnail_val-product-option').attr('src', value.cell.image_thumbnail);
-					$('#image_thumbnail_link-product-option').attr('href', value.cell.image_thumbnail);
-					$('#image_thumbnail_link-product-option span').html(value.cell.thumb);
-					
-					$('#image_small_val-product-option').attr('src', value.cell.image_small);
-					$('#image_small_link-product-option').attr('href', value.cell.image_small);
-					$('#image_small_link-product-option span').html(value.cell.small);
-					
-					$('#image_large_val-product-option').attr('src', value.cell.image_large);
-					$('#image_large_link-product-option').attr('href', value.cell.image_large);
-					$('#image_large_link-product-option span').html(value.cell.large);
-					
-					$('#thumbnail_current-product-option').attr('value', value.cell.image_thumbnail);
-					$('#small_current-product-option').attr('value', value.cell.image_small);
-					$('#large_current-product-option').attr('value', value.cell.image_large);
-					
-					$('#price-product-option').attr('value', value.cell.price);
-					$('#weight-product-option').attr('value', value.cell.weight);
-					$('#product-id').attr('value',value.cell.product_id);
+					$('#submitter').attr('value', 'Update');
+					$('#action').attr('value', 'update');
+					$('#modal-create-orders').modal('show');
 				});
-},
-dataType:'json'
-});
-$('#product-id').attr('value',$('.flexProducts .trSelected').attr('id').substr(3));
-$('#products-option-id').attr('value',$(this).attr('id').substr(3));
-$('#myTab li:eq(2) a').tab('show');
-$('#delete-product-option').show();
-$('#submitter-product-option').attr('value', 'Update');
-$('#action-product-option').attr('value', 'update');
-});
-});
+			});
 
-$('#myTab li:eq(1) a').on('shown', function () {
-	$('#product-id').attr('value',$('.flexOrders .trSelected').attr('id').substr(3));
-	$('#submitter-product-option').attr('value', 'Create');		
-	$('#action-product-option').attr('value', 'create');
-	$('#delete-product-option').hide();
-	
-	$('#option_name').attr('value', '');
-	$('#option_description').attr('value', '');
-	
-	$('#image_thumbnail_val-product-option').attr('src', '');
-	$('#image_small_val-product-option').attr('src', '');
-	$('#image_large_val-product-option').attr('src', '');
-	
-	$('#thumbnail_current-product-option').attr('value', '');
-	$('#small_current-product-option').attr('value', '');
-	$('#large_current-product-option').attr('value', '');
-	
-	$('#image_small_link-product-option span').html('');
-	$('#image_large_link-product-option span').html('');
-	$('#image_thumbnail_link-product-option span').html('');
-	
-	$('#price-product-option').attr('value', '');       
-	$('#weight-product-option').attr('value', ''); 
-        //$('#product-id-option').attr('value', ''); 
-    });
-});
+			$('#modal-create-orders').on('shown', function () {
+				
+				if( $('#submitter').attr('value') == 'Add' || $('#submitter').attr('value') == 'Create' ) {
+					$('#order-details-select').hide();
+					$('#order-items-select').hide();
+					$('#myTab li:eq(0) a').tab('show');
+					$('#myTab li:eq(1) a').tab('hide');
+					$('#myTab li:eq(2) a').tab('hide');
+				} else if( $('#submitter').attr('value') == 'Update') {
+					$('#order-details-select').show();
+					$('#order-items-select').show();
+				}
+			})
 
-</script>
+			$('#modal-create-orders').on('hidden', function () {
+				window.location.reload();
+				$('#myTab li:eq(0) a').tab('show');
+				
+				$('#submitter').attr('value', 'Create');		
+				$('#user-id').attr('value', '');
+				$('#action').attr('value', 'create');
+				$('#delete').hide();
+				
+				$('#name_short').attr('value', '');
+				$('#name_long').attr('value', '');
+				$('#description_long').text('');
+				$('#description_short').attr('value', '');
+				
+				$('#image_thumbnail_val').attr('src', '');
+				$('#image_small_val').attr('src', '');
+				$('#image_large_val').attr('src', '');
+				
+				$('#thumbnail_current').attr('value', '');
+				$('#small_current').attr('value', '');
+				$('#large_current').attr('value', '');
+				
+				$('#image_small_link span').html('');
+				$('#image_large_link span').html('');
+				$('#image_thumbnail_link span').html('');
+				
+				$('#comments').attr('value', '');
+				$('#name_long').attr('value', '');
+				$('#comments').text('');
+				$('#zip').attr('value', '');
+				$('#country').attr('value',  '');
+				
+				$('#status').val('');
+				
+				$('#price').attr('value', '');
+				
+				$('#category_id').val('');
+				$('#brand_id').val('');
+				$('#vendor_id').val('');
+			});
+
+			//on click product options 		
+			$('.flexProductOptions').click(function(event){
+				console.log('Tab clicked');
+				$('.trSelected', this).each( function(){	
+					$.ajax({
+						type:'POST',
+						url:"core/view/grid/productOptions.php",
+						data:"id="+$(this).attr('id').substr(3),
+						success:function(data) {
+							$.each(data.rows, function (index, value) {	
+								$('#option_name').attr('value', value.cell.option_name);
+								$('#option_description').attr('value', value.cell.option_description);
+								
+								$('#image_thumbnail_val-product-option').attr('src', value.cell.image_thumbnail);
+								$('#image_thumbnail_link-product-option').attr('href', value.cell.image_thumbnail);
+								$('#image_thumbnail_link-product-option span').html(value.cell.thumb);
+								
+								$('#image_small_val-product-option').attr('src', value.cell.image_small);
+								$('#image_small_link-product-option').attr('href', value.cell.image_small);
+								$('#image_small_link-product-option span').html(value.cell.small);
+								
+								$('#image_large_val-product-option').attr('src', value.cell.image_large);
+								$('#image_large_link-product-option').attr('href', value.cell.image_large);
+								$('#image_large_link-product-option span').html(value.cell.large);
+								
+								$('#thumbnail_current-product-option').attr('value', value.cell.image_thumbnail);
+								$('#small_current-product-option').attr('value', value.cell.image_small);
+								$('#large_current-product-option').attr('value', value.cell.image_large);
+								
+								$('#price-product-option').attr('value', value.cell.price);
+								$('#weight-product-option').attr('value', value.cell.weight);
+								$('#product-id').attr('value',value.cell.product_id);
+							});
+						},
+						dataType:'json'
+					});
+					$('#product-id').attr('value',$('.flexProducts .trSelected').attr('id').substr(3));
+					$('#products-option-id').attr('value',$(this).attr('id').substr(3));
+					$('#myTab li:eq(2) a').tab('show');
+					$('#delete-product-option').show();
+					$('#submitter-product-option').attr('value', 'Update');
+					$('#action-product-option').attr('value', 'update');
+				});
+			});
+
+			$('#myTab li:eq(1) a').on('shown', function () {
+				$('#product-id').attr('value',$('.flexOrders .trSelected').attr('id').substr(3));
+				$('#submitter-product-option').attr('value', 'Create');		
+				$('#action-product-option').attr('value', 'create');
+				$('#delete-product-option').hide();
+				
+				$('#option_name').attr('value', '');
+				$('#option_description').attr('value', '');
+				
+				$('#image_thumbnail_val-product-option').attr('src', '');
+				$('#image_small_val-product-option').attr('src', '');
+				$('#image_large_val-product-option').attr('src', '');
+				
+				$('#thumbnail_current-product-option').attr('value', '');
+				$('#small_current-product-option').attr('value', '');
+				$('#large_current-product-option').attr('value', '');
+				
+				$('#image_small_link-product-option span').html('');
+				$('#image_large_link-product-option span').html('');
+				$('#image_thumbnail_link-product-option span').html('');
+				
+				$('#price-product-option').attr('value', '');       
+				$('#weight-product-option').attr('value', ''); 
+			        //$('#product-id-option').attr('value', ''); 
+		    });
+		});
+
+	</script>
+</div>
